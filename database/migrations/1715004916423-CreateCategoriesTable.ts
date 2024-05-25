ce5072f7913e57a25c22d74d37eda5a5b03c52f1
Juan David Garcia <juancho637@gmail.com>
2024-05-24 22:54:18 -0500

    chore: add base folders to modules auth, categories, counters, customers, permissions, providers, roles and users

---
+++ database/migrations/1715004916423-CreateCategoriesTable.ts
@@ -0,0 +1,57 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateCategoriesTable1715004916423 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'categories',
+        columns: [
+          {
+            name: 'id',
+            type: 'integer',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '50',
+            isNullable: false,
+          },
+          {
+            name: 'slug',
+            type: 'varchar',
+            length: '100',
+            isNullable: false,
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp with time zone',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp with time zone',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp with time zone',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('categories');
+  }
+}
