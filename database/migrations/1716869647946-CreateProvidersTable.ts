82dd714a278cef52f4b041ca2cdef632b20433cc
Cristian Gómez <89625031+ccgg1997@users.noreply.github.com>
2024-05-28 04:22:42 +0000

    migracion de provider

---
+++ database/migrations/1716869647946-CreateProvidersTable.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateProvidersTable1716869647946 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'providers',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+          },
+          {
+            name: 'status',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'deleted_at',
+            type: 'timestamp',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('providers');
+  }
+}
