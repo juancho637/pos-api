2034ab7bbe77c00b85ed57917f83f482d41d4b86
Juan David Garcia <juancho637@gmail.com>
2025-01-03 19:43:42 -0500

    export TypeOrmModule in DatabaseModule

--- src/common/adapters/database/infrastructure/database.module.ts
+++ src/common/adapters/database/infrastructure/database.module.ts
@@ -14,5 +14,6 @@ import { TypeOrmConfigService } from './typeorm-config.service';
       useClass: TypeOrmConfigService,
     }),
   ],
+  exports: [TypeOrmModule],
 })
 export class DatabaseModule {}
